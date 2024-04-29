551da56fc816d44df97a61934d80886206cce0c7
BaLEE <71264141+tidik@users.noreply.github.com>
2024-04-29 16:50:05 +0800

    modified:   scripts/3gwap_bd.js

--- scripts/3gwap_bd.js
+++ scripts/3gwap_bd.js
@@ -67,6 +67,7 @@ function _writeHttpHeader() {
   let conPort = $session.conPort;
   let verify = createVerify(conHost);
 
-  var header = `CONNECT ${conHost}:${conPort}${tag}${fake_host} HTTP/1.1\r\nHost: ${fake_host}\r\nX-T5-Auth: ${verify}\r\nProxy-Connection: keep-alive\r\n\r\n`;
+  var header = `CONNECT ${conHost}:${conPort}${tag}${fake_host} HTTP/1.1\r\nHost: ${fake_host}@${conHost}\r\nX-T5-Auth: ${verify}\r\nProxy-Connection: keep-alive\r\n\r\n`;
   $tunnel.write($session, header);
+  console.log(header);
 }
